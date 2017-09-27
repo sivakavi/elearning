371656509ed7ae2fcfaa1d8d4f78bcce11ba1098
Candy <candy.rpk123@gmail.com>
2017-09-27 17:15:06 +0530

    [edit] student dashboard work

--- resources/views/student/dashboard.blade.php
+++ resources/views/student/dashboard.blade.php
@@ -38,6 +38,81 @@
                     <p>No. of Viewed Lessions.</p>
                     </div>
                 </div>
+    </div>  
+
+    <div class="row margin-top-50">
+
+    <div class="col-sm-12">
+              <div class="x_panel tile">
+                <div class="x_title">
+                  <h2>Lessions Status</h2>
+                  <ul class="nav navbar-right">
+                  <li class="cursor-pointer"><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                  </li>
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+
+                    <div class="row">
+                        <div class="col-md-8 col-sm-12 col-xs-12 center-margin">
+                            <div class="widget_summary">
+                                <div class="w_left w_25">
+                                <span>Category 1</span>
+                                </div>
+                                <div class="w_center w_55">
+                                <div class="progress">
+                                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 66%;">
+                                        60%
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="w_right w_20">
+                                <span class="font-size-15">60%</span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+
+                            <div class="widget_summary">
+                                <div class="w_left w_25">
+                                <span>Category 2</span>
+                                </div>
+                                <div class="w_center w_55">
+                                <div class="progress">
+                                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 45%;">
+                                        70%
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="w_right w_20">
+                                <span class="font-size-15">60%</span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+
+                            <div class="widget_summary">
+                                <div class="w_left w_25">
+                                <span>Category 3</span>
+                                </div>
+                                <div class="w_center w_55">
+                                <div class="progress">
+                                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                                        60%
+                                    </div>
+                                </div>
+                                </div>
+                                <div class="w_right w_20">
+                                <span class="font-size-15">60%</span>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div>
+                        </div>
+                    </div>
+                  
+                </div>
+              </div>
+            </div>
+
     </div>
 
     <div class="row margin-top-50">
@@ -75,6 +150,9 @@
                 </div>
               </div>
     </div>
+    <br/>
+    <br/>
+    <br/>
 
 @endsection
 
